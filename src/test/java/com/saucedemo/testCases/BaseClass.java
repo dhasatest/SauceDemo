ca795e0a3461be6fbf6b79479f3dc470545e4917
Dhasa <dhasa@ymail.com>
2022-05-01 20:23:50 +0530

    updated

--- src/test/java/com/saucedemo/testCases/BaseClass.java
+++ src/test/java/com/saucedemo/testCases/BaseClass.java
@@ -12,8 +12,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -40,13 +39,13 @@ public class BaseClass {
 		logger = Logger.getLogger("SauceDemo");
 		PropertyConfigurator.configure("Log4j.properties");
 		
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
-		driver = new ChromeDriver();
+		//WebDriverManager.chromedriver().setup();
+		//ChromeOptions options = new ChromeOptions();
+		//options.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+		//driver = new ChromeDriver();
 		
-		//WebDriverManager.firefoxdriver().setup();
-		//driver = new FirefoxDriver();
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
 		
 		logger.info("Opening Url : " + url);
 		driver.get(url);

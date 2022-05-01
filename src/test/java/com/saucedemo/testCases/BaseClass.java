f8c4ab8865b64405c0954135c877d70a0ea9d9d3
Dhasa <dhasa@ymail.com>
2022-05-01 20:21:15 +0530

    updated

--- src/test/java/com/saucedemo/testCases/BaseClass.java
+++ src/test/java/com/saucedemo/testCases/BaseClass.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -40,7 +41,12 @@ public class BaseClass {
 		PropertyConfigurator.configure("Log4j.properties");
 		
 		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
 		driver = new ChromeDriver();
+		
+		//WebDriverManager.firefoxdriver().setup();
+		//driver = new FirefoxDriver();
 		
 		logger.info("Opening Url : " + url);
 		driver.get(url);
